1e8e3f9af8bef7737f134bf85b2b08422cb96f15
dillon <dillon.ward@stellar.tc>
2025-07-25 13:30:24 +0200

    processing and storing data

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from src.data.processor import unzip_data_extract_contents, extract_subfolders
-
+from src.data.processor import unzip_data_extract_contents
 
 def main():
-    
-    train_folders, test_folders, top_level_files = unzip_data_extract_contents()
-    print("Train Folders:", train_folders)
-    print("Test Folders:", test_folders)
 
-    test = extract_subfolders(test_folders)
-    print("Test Subfolders:", test)
-    # for folder in train_folders:
-    #     subfolders = extract_subfolders(folder)
-    #     print(f"Subfolders in {folder}:", subfolders)
+    test_archive, train_archive, unsup_archive, imdb_vocab, imdb_expected_rating = unzip_data_extract_contents()
 
 main()
 
