3a45c18264dbd5f58d7a496e28a5de04197c43b0
dillon <dillon.ward@stellar.tc>
2025-07-25 21:19:17 +0200

    making predictions

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from src.data.processor import unzip_data_extract_contents
 from src.model.train_model import train_bow_logreg, train_tfidf_logreg
-
+from src.model.predict_model import predict_sentiment_tfidf, predict_sentiment_bow
 
 def main():
     test_archive, train_archive, unsup_archive, imdb_vocab, imdb_expected_rating = unzip_data_extract_contents()
-    train_tfidf_logreg(test_archive, train_archive, imdb_vocab, imdb_expected_rating)
-    train_bow_logreg(test_archive, train_archive, imdb_vocab)
+    # train_tfidf_logreg(test_archive, train_archive, imdb_vocab, imdb_expected_rating)
+    # train_bow_logreg(test_archive, train_archive, imdb_vocab)
+
+    sample_text = "this movie was mid"
+    print("TFIDF Prediction:", predict_sentiment_tfidf(sample_text))
+    print("BoW Prediction:", predict_sentiment_bow(sample_text, imdb_vocab))
+
+    sample_text = "it wasnt bad"
+    print("TFIDF Prediction:", predict_sentiment_tfidf(sample_text))
+    print("BoW Prediction:", predict_sentiment_bow(sample_text, imdb_vocab))
 
 
 main()
