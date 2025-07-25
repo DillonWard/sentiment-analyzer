2b103aab776002a97e52ca42ba3fdd0bb6bf3578
dillon <dillon.ward@stellar.tc>
2025-07-25 16:21:08 +0200

    training model and making predictions

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,7 +1,7 @@
 import json
 import os
 import gzip
-
+import joblib
 
 def export_data_to_json(data, file_name):
     project_root = os.path.dirname(
@@ -29,4 +29,25 @@ def import_processed_json(file_name):
     with gzip.open(path, 'rt', encoding='utf-8') as f:
         data = json.load(f)
     
+    return data
+
+def export_models(data, file_name):
+    project_root = os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    )
+    path = os.path.join(project_root, "models", file_name)
+
+    import joblib
+    joblib.dump(data, path)
+
+def import_models(file_name):
+    project_root = os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    )
+    path = os.path.join(project_root, "models", file_name)
+
+    if not os.path.exists(path):
+        return None
+
+    data = joblib.load(path)
     return data
