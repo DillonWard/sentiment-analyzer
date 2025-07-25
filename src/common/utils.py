fc8980bdbf42e5d5dc289091f58c413e8f7cf65d
dillon <dillon.ward@stellar.tc>
2025-07-25 13:38:51 +0200

    storing json in zipped format

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,33 +1,32 @@
 import json
 import os
-from src.data.archive import Archive
+import gzip
 
-def export_data_to_json(archive, file_name):
-    """
-    Exports the reviews from an Archive object to a JSON file.
-    """
+
+def export_data_to_json(data, file_name):
     project_root = os.path.dirname(
         os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     )
+    if not file_name.endswith('.gz'):
+        file_name += '.gz'
     path = os.path.join(project_root, "data", "processed", file_name)
 
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(archive, f, ensure_ascii=False, indent=2)
-
+    with gzip.open(path, 'wt', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+        
 
 def import_processed_json(file_name):
-    """
-    Imports reviews from a JSON file into an Archive object.
-    """
     project_root = os.path.dirname(
         os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     )
+    if not file_name.endswith('.gz'):
+        file_name += '.gz'
     path = os.path.join(project_root, "data", "processed", file_name)
 
     if not os.path.exists(path):
         return None
 
-    with open(path, 'r', encoding='utf-8') as f:
+    with gzip.open(path, 'rt', encoding='utf-8') as f:
         data = json.load(f)
     
     return data
